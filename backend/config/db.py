4e9b115628e3e391370f07cd41fd04a0873ee5d2
joaomachado-mack <10337601@mackenzista.com.br>
2024-11-09 22:53:33 -0300

    Delete backend directory

--- backend/config/db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-client = MongoClient(os.getenv("MONGODB_URI"))
-db = client["seu_professor"]
--- backend/services/ai_service.py
+++
@@ -1,3 +0,0 @@
-def get_ai_response(question):
-    # Exemplo de resposta simples
-    return f"Aqui está a resposta da IA para a pergunta: {question}"
